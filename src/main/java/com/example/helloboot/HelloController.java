af2b180ee2af0b4775b7e61f03fc9ae9167043e2
JE1000\CJE <je1000@com2us.com>
2023-03-08 12:05:37 +0900

    SpringBoot Helloboot

---
+++ src/main/java/com/example/helloboot/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.helloboot;
+
+import java.util.Objects;
+
+public class HelloController {
+
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    public String hello(String name){
+        return helloService.sayHello(Objects.requireNonNull(name));
+    }
+}
