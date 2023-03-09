3af5a591373f18f1391efb34fe969f2c85cb8805
JE1000\CJE <je1000@com2us.com>
2023-03-09 11:14:24 +0900

    how to test the code

--- src/test/java/com/example/helloboot/HelloApiTest.java
+++ src/test/java/com/example/helloboot/HelloApiTest.java
@@ -1,3 +1,5 @@
+package com.example.helloboot;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -17,7 +19,7 @@ public class HelloApiTest {
                 rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
 
         // status code 200
-        Assertions.assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(res.getStatusCode()) .isEqualTo(HttpStatus.OK);
         // header(content-type) text/plain
         Assertions.assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
         // body Hello Spring
--- src/test/java/com/example/helloboot/HellobootApplicationTests.java
+++ src/test/java/com/example/helloboot/HellobootApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.helloboot;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class HellobootApplicationTests {
 
 	@Test
 	void contextLoads() {
+		SimpleHelloService helloService = new SimpleHelloService();
+
+		String ret = helloService.sayHello("Test");
+
+		Assertions.assertThat(ret).isEqualTo("Hello Test");
 	}
 
 }
