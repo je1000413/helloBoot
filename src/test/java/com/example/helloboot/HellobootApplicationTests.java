5abc874d5c47ae46368dae051135a2bc7dd9f1b5
JE1000\CJE <je1000@com2us.com>
2023-03-09 11:33:23 +0900

    Spring Container/Decorator

--- src/test/java/com/example/helloboot/HellobootApplicationTests.java
+++ src/test/java/com/example/helloboot/HellobootApplicationTests.java
@@ -15,4 +15,12 @@ class HellobootApplicationTests {
 		Assertions.assertThat(ret).isEqualTo("Hello Test");
 	}
 
+	@Test
+	void helloDecorator(){
+		HelloDecorator decorator = new HelloDecorator(name -> name);
+
+		String ret = decorator.sayHello("Test");
+		Assertions.assertThat(ret).isEqualTo("*Test*");
+	}
+
 }
