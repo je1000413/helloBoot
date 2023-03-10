62813840830ec28032ee18b71a38b895028e10ac
JE1000\CJE <je1000@com2us.com>
2023-03-10 15:18:44 +0900

    Auto Configuration: test

--- src/main/java/com/example/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/example/config/autoconfig/TomcatWebServerConfig.java
@@ -1,20 +1,16 @@
-package com.example.helloboot;
+package com.example.config.autoconfig;
 
+import com.example.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
-public class Config {
+@MyAutoConfiguration
+public class TomcatWebServerConfig {
     @Bean
     public ServletWebServerFactory servletWebServerFactory(){
         return new TomcatServletWebServerFactory();
-    }
-
-    @Bean
-    public DispatcherServlet dispatcherServlet(){
-        return new DispatcherServlet();
     }
 }
--- src/main/java/com/example/helloboot/HellobootApplication.java
+++ src/main/java/com/example/helloboot/HellobootApplication.java
@@ -1,14 +1,9 @@
 package com.example.helloboot;
 
+import com.example.config.MySpringBootApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
 
-@MySpringBootAnnotation
+@MySpringBootApplication
 public class HellobootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HellobootApplication.class, args);
