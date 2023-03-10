cbb941cfb2d13813dcdd0c7efea3cd4508a4fccc
JE1000\CJE <je1000@com2us.com>
2023-03-10 11:10:13 +0900

    Auto Configuration: Annotation Customize

--- src/test/java/com/example/helloboot/HellobootApplicationTests.java
+++ src/test/java/com/example/helloboot/HellobootApplicationTests.java
@@ -4,9 +4,25 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@UnitTest
+@interface FastUnitTest{
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Test
+@interface UnitTest{
+}
+
 class HellobootApplicationTests {
 
-	@Test
+	@UnitTest
 	void contextLoads() {
 		SimpleHelloService helloService = new SimpleHelloService();
 
