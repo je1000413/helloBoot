69ff83d2a5120a8ebbbb073fa0205071a69b566a
JE1000\CJE <je1000@com2us.com>
2023-03-10 11:14:46 +0900

    Auto Configuration: HelloBootApplication 초기 모델과 동일 구성

--- src/test/java/com/example/helloboot/HellobootApplicationTests.java
+++ src/test/java/com/example/helloboot/HellobootApplicationTests.java
@@ -8,21 +8,10 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-@UnitTest
-@interface FastUnitTest{
-}
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
-@Test
-@interface UnitTest{
-}
 
 class HellobootApplicationTests {
 
-	@UnitTest
+	@Test
 	void contextLoads() {
 		SimpleHelloService helloService = new SimpleHelloService();
 
